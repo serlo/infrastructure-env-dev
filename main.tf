51947e0bbb0f688dd074e8b1dc90bde6705627c2
Richard Mörbitz <richard.moerbitz@tu-dresden.de>
2019-10-13 20:17:41 +0200

    kpi: update module version

--- main.tf
+++ main.tf
@@ -208,7 +208,7 @@ module "kpi_metrics" {
 }
 
 module "kpi" {
-  source = "github.com/serlo/infrastructure-modules-kpi.git//kpi?ref=v1.1.0"
+  source = "github.com/serlo/infrastructure-modules-kpi.git//kpi?ref=v1.2.0"
   domain = local.domain
 
   grafana_admin_password = var.kpi_grafana_admin_password
