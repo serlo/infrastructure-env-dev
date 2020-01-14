90092d3f4e9f85a1f730e36e9977dc0f25fb74dd
Jonas Keinholz <git@inyono.dev>
2020-01-14 16:48:54 +0100

    update serlo.org

--- main.tf
+++ main.tf
@@ -17,9 +17,9 @@ locals {
       php               = "5.2.2"
       notifications_job = "2.0.1"
     }
-    editor_renderer        = "4.0.1"
+    editor_renderer        = "4.0.2"
     legacy_editor_renderer = "2.0.0"
-    frontend               = "2.0.4"
+    frontend               = "2.0.5"
   }
   varnish_image = "eu.gcr.io/serlo-shared/varnish:6.0"
 
@@ -211,7 +211,7 @@ module "serlo_org" {
 
     upload_secret   = file("secrets/serlo-org-6bab84a1b1a5.json")
     hydra_admin_uri = module.hydra.admin_uri
-    feature_flags   = "['client-frontend' => true, 'donation-banner' => true, 'frontend-content' => true, 'frontend-diff' => true, 'frontend-donation-banner' => true, 'frontend-editor' => true, 'frontend-footer' => true, 'frontend-legacy-content' => true, 'key-value-store' => true]"
+    feature_flags   = "['client-frontend' => true, 'frontend-content' => true, 'frontend-diff' => true, 'frontend-editor' => true, 'frontend-footer' => true, 'frontend-legacy-content' => true, 'key-value-store' => true]"
     redis_hosts     = "['redis-master.redis']"
   }
 
