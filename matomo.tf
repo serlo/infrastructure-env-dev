927c6e4af49a20f49f27bcb0d64a854fc44e22c9
Jonas Keinholz <git@inyono.dev>
2020-04-15 06:38:29 +0200

    update cloudflare & matomo module
    # Conflicts:
    #	main.tf

--- matomo.tf
+++ matomo.tf
@@ -5,7 +5,7 @@ locals {
 }
 
 module "matomo" {
-  source = "github.com/serlo/infrastructure-modules-shared//matomo?ref=db7232c2d87e3962293280119a507eafbc7e267f"
+  source = "github.com/serlo/infrastructure-modules-shared//matomo?ref=e4b728cedce1c6b3d1973028c60e6157411ef3e6"
 
   app_replicas  = 1
   image_tag     = local.matomo.matomo_image_tag
@@ -25,7 +25,7 @@ resource "kubernetes_namespace" "matomo_namespace" {
 }
 
 module "matomo_ingress" {
-  source = "github.com/serlo/infrastructure-modules-shared.git//ingress?ref=c41476e253475fa2eacbada4228074dd6d7df58f"
+  source = "github.com/serlo/infrastructure-modules-shared.git//ingress?ref=e4b728cedce1c6b3d1973028c60e6157411ef3e6"
 
   name      = "matomo"
   namespace = kubernetes_namespace.matomo_namespace.metadata.0.name
