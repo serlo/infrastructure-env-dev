09eaa42e6c2ed65cec4e37edd741ccfe9e49aca8
Richard Mörbitz <richard.moerbitz@tu-dresden.de>
2020-08-13 22:50:07 +0200

    kpi: bump aggregator version

--- kpi.tf
+++ kpi.tf
@@ -2,7 +2,7 @@ locals {
   kpi = {
     grafana_image_tag        = "1.6.0"
     mysql_importer_image_tag = "1.4.1"
-    aggregator_image_tag     = "1.6.4"
+    aggregator_image_tag     = "1.7.0"
     mfnf_importer_image_tag  = "1.0.0"
   }
 }
